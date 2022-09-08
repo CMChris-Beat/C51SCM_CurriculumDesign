da80591fb3e6637e92d7e49c8fc87406f6469797
樱落Eglo <452678184@qq.com>
2022-09-08 09:16:03 +0800

    [Sf]Update the program, complete

--- 3.Software/1.Keil5_C52RC/Source/main.c
+++ 3.Software/1.Keil5_C52RC/Source/main.c
@@ -3,9 +3,10 @@
 #define uchar unsigned char
 #define uint unsigned int
 #define TIME 26
+#define RES 10000
 
-uchar code dis_code[]={0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10};
-uchar code zhc[]={0x10,0x79,0x40,0x79,0x19,0x79,0x24,0x12};
+uchar code dis_code[]={0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90};
+uchar code dis_code_point[]={0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10};
 uchar buf[10] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a};
 uchar array[] = {0x7f, 0x3f, 0x1f, 0x0f, 0x07, 0x03, 0x01, 0x00, 0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe, 0xff};
 
@@ -109,57 +110,46 @@ void Key2() {
 void Key3() {
 	int i;
 	for (i = 0; i < 1025; i+=4) {
+		if (K3) DA_OUTPUT(0);
 		if ((short int)K1 + (short int)K2 + (short int)K3 + (short int)K4 != 3) break;
 		DA_OUTPUT(i);
 		delay(20);
 	}
 	for (i = 1024; i >= 0; i-=4) {
+		if (K3) DA_OUTPUT(0);
 		if ((short int)K1 + (short int)K2 + (short int)K3 + (short int)K4 != 3) break;
 		DA_OUTPUT(i);
 		delay(20);
 	}
-//	DA_OUTPUT(255);
 	delay(100);
 }
 
-//void Key3() {
-//	uchar l;
-//	int temp;
-//	P2 = 0x80;
-//	temp = P2;
-//	for(l=0; l<4; l++) {
-//		temp = _crol_(temp,1);
-//		P2 = temp & 0x0f | 0xf0;
-//		P1=zhc[l];
-//		delay(2);
-//		P1=0xff;
-//	}
-//	P1 = 0xff;
-//}
-
 void Key4() {
-	uint result;
-	uint num[4];
+	int result;
+	uchar num[4];
 	uchar l;
 	int temp;
 	CS2=1;
 	SCLK=0;
 	SDO=1;
 	while(!K4) {
-		result = TLC549_Convert();
+		result = RES / 255 * TLC549_Convert();
 		num[0] = result / 1000;
 		num[1] = (result - num[0]*1000) / 100;
 		num[2] = (result - num[0]*1000 - num[1]*100) / 10;
 		num[3] = result % 10;
-		P2 = 0x80;
-		temp = P2;
+		temp = 0x80;
 		for(l=0; l<4; l++) {
 			delay(2);
 			temp = _crol_(temp,1);
-			P2 = temp & 0xff | 0x0f;
-			P1 = dis_code[num[l]];
+			P2 = temp | 0xf0;
+			if(l < 3) {
+				P1 = dis_code[num[l]];
+			}else {
+				P1 = dis_code_point[num[l]];
+			}
 			delay(2);
-			P1=0xff;
+			P1 = 0xff;
 		}
 		P1 = 0xff;
 	}
@@ -176,6 +166,7 @@ void timer1() interrupt 1{
 	}
 	if (K2 == 1) {
 		P0 = 0xff;
+		k = 200 * TIME;
 		TR0 = 0;
 	}
 }
